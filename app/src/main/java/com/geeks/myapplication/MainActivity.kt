f8af0a36556ebf17eae706d0b443dfe33f659684
DanielToktobaev <toktobaevdaniel0@gmail.com>
2024-12-27 20:14:08 +0600

    ii

--- app/src/main/java/com/geeks/myapplication/MainActivity.kt
+++ app/src/main/java/com/geeks/myapplication/MainActivity.kt
@@ -11,7 +11,7 @@ import com.geeks.myapplication.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onResume(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
